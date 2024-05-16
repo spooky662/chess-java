075485ffb05afa257ec2a603898e574c0c55c689
Spooky662 <104535161+spooky662@users.noreply.github.com>
2024-05-15 21:05:46 -0300

    Update ChessMatch.java

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,12 +1,17 @@
-package Chess;
+
+package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -17,5 +22,15 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    private void initialSetup() {
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
